9a499b0124646c2b7cd5c9afc4257056818cbd6b
Laura <laura.garcia07@usa.edu.co>
2024-03-08 02:35:12 +0000

    Se agrega función de descarga

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ec2-user/environment/env"
+set -gx VIRTUAL_ENV "/home/ec2-user/environment/Parcial1Corte-BigData/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
